2b55ea53f446038f8cefaad4910cf97e45d40434
Shumin Liu <shumin.liu@tngtech.com>
2022-06-10 15:42:25 +1000

    Refactor: use array in formatter

--- display.ts
+++ display.ts
@@ -13,16 +13,13 @@ export class NodeFormatter {
   private lineFeed = "\n";
 
   public nodeToCliString(roots: Roots): string {
-    let res = roots.roots
-      .map((root) => this.formatNode(0, root))
-      .reduce((a, b) => a + b, "");
-    if (roots.roots.length < roots.count) {
-      res += `... (${roots.count} entries in total)` + this.lineFeed;
-    }
-    return res;
+    return [
+      ...roots.roots.map((root) => this.formatNode(0, root)).flat(),
+      `... (${roots.count} entries in total)`,
+    ].join(this.lineFeed);
   }
 
-  private formatNode(level: number, node: Node): string {
+  private formatNode(level: number, node: Node): string[] {
     if (node.type === "folder") {
       return this.formatFolder(level, node);
     } else if (node.type === "file") {
@@ -34,21 +31,26 @@ export class NodeFormatter {
     }
   }
 
-  private formatFile(level: number, node: FileNode): string {
-    return this.spaces(level) + rgb24(node.path, this.fileNodeColor) +
-      this.lineFeed;
+  private formatFile(level: number, node: FileNode): string[] {
+    return [
+      this.spaces(level) + rgb24(node.path, this.fileNodeColor),
+    ];
   }
 
-  private formatNotExistNode(level: number, node: NotExistNode): string {
-    return this.spaces(level) +
-      rgb24(node.path + " [Not exist]", this.notExistNodeColor) + this.lineFeed;
+  private formatNotExistNode(level: number, node: NotExistNode): string[] {
+    return [
+      this.spaces(level) +
+      rgb24(node.path + " [Not exist]", this.notExistNodeColor),
+    ];
   }
 
-  private formatFolder(level: number, node: FolderNode): string {
-    return this.spaces(level) +
+  private formatFolder(level: number, node: FolderNode): string[] {
+    return [
+      this.spaces(level) +
       rgb24(node.path, this.folderNodeColor) +
-      ` (${node.childrenCount} in total)` + this.lineFeed +
-      node.children.map((n) => this.formatNode(level + 1, n));
+      ` (${node.childrenCount} in total)`,
+      ...node.children.map((n) => this.formatNode(level + 1, n)).flat(),
+    ];
   }
 
   private spaces(level: number): string {
